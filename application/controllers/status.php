babf430f999bdc402ed3c5cc1f047f32b85617a2
Ken Auberry <kenneth.auberry@pnnl.gov>
2015-01-29 16:10:47 -0800

    Working on intial status reporting

--- application/controllers/status.php
+++ application/controllers/status.php
@@ -8,8 +8,8 @@ class Status extends Baseline_controller {
     if($this->admin_access_level < 400){
       $this->page_data['message'] = "You must have at least 'Power User' status to use these pages";
       $this->load->view('insufficient_privileges', $this->page_data);
-      // $this->load->model('status_model','status');
     }
+    $this->load->model('status_model','status');
     $this->load->helper(array('inflector','url','opwhse_search','form'));
     $this->load->library(array('table'));
   }
@@ -17,6 +17,10 @@ class Status extends Baseline_controller {
   public function index(){
     $this->page_data['page_header'] = "MyEMSL Status Reporting";
     $this->page_data['title'] = "Overview";
+    
+    $instrument_group_xref = $this->status->get_instrument_group_list();
+    
+    
     $this->load->view('emsl_mgmt_view',$this->page_data);
   }
   
--- application/models/status_model.php
+++ application/models/status_model.php
@@ -1,3 +1,54 @@
 <?php
-    phpinfo();
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                             */
+/*     Navigation Info Model                                                   */
+/*                                                                             */
+/*             functionality for setting up left hand nav menu                 */
+/*                                                                             */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+class Status_model extends CI_Model {
+  function __construct(){
+    parent::__construct();
+
+  }
+  
+  
+  /*
+   * gathers wide overview data about many types of instruments for upper-level mgmt
+   * 
+   */
+  function get_status_overview($class_filter = "", $instrument_filter = ""){
+    
+  }
+  
+  function get_status_for_instrument_before_date($instrument_list, $start_time){
+    if(is_string($instrument_list)){
+      $instrument_list = explode(',',$instrument_list);
+    }
+    
+    
+    
+    
+    
+  }
+  
+  function get_status_for_instrument_over_range($instrument_list, $time_period = "1 day"){
+    $current_time_obj = new DateTime();
+    $current_time_obj->setTime($current_time_obj->getHours(),0,0);
+    $start_time = date_modify($current_time_obj, "-{$time_period}");
+    return $this->get_status_for_instrument_before_date($instrument_list, $start_time);
+    
+  }
+  
+  function get_instrument_group_list($filter = ""){
+    $json_string = file_get_contents(FCPATH."resources/json_files/instrument_group_list.json");
+    $inst_list = array();
+    $inst_list_raw = json_decode($json_string,TRUE);
+    foreach($inst_list_raw['RECORDS'] as $item){
+      $inst_list[$item['group_id']] = $item['name'];
+    }
+    return $inst_list;
+  }
+  
+}
 ?>
