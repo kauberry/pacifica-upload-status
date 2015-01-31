92ec3cec505d455e89e2adf1662b6e7c1554cd28
Ken Auberry <kenneth.auberry@pnnl.gov>
2015-01-30 16:56:06 -0800

    Working on status overview page

--- application/controllers/status.php
+++ application/controllers/status.php
@@ -17,10 +17,14 @@ class Status extends Baseline_controller {
   public function index(){
     $this->page_data['page_header'] = "MyEMSL Status Reporting";
     $this->page_data['title'] = "Overview";
+    $this->page_data['css_uris'] = array(base_url()."resources/stylesheets/status.css");
     
     $instrument_group_xref = $this->status->get_instrument_group_list();
     
-    
+    // echo "<pre>";
+    // var_dump($instrument_group_xref);
+    // echo "</pre>";
+    $this->page_data['instrument_list'] = $instrument_group_xref;
     $this->load->view('emsl_mgmt_view',$this->page_data);
   }
   
--- application/views/emsl_mgmt_view.php
+++ application/views/emsl_mgmt_view.php
@@ -10,6 +10,43 @@
       <div id="main">
         
         <h1 class="underline"><?= $page_header ?></h1>
+        <div style="position:relative;">
+          <div class="form_container">
+            <form id="instrument_selection" class="themed">
+              <fieldset id="inst_select_container">
+                <legend>Instrument Selection</legend>
+                <div class="full_width_block">
+                  <div class="left_block">
+                    <select>
+                      <option value>Select an Instrument...</option>
+                    <?php foreach($instrument_list as $inst_id => $inst_name): ?>
+                      <option value="<?= $inst_id ?>"><?= $inst_name ?></option>
+                    <?php endforeach; ?>
+                    </select>
+                  </div>
+                  <div class="right_block">
+                    <select style="width:100%;">
+                      <option value>Select a Time Frame...</option>
+                      <option value="-1 day">Last 24 Hours</option>
+                      <option value="-7 days">Last 7 Days</option>
+                      <option value="-1 month">Last Month</option>
+                      <option value="-1 year">Last Year</option>
+                    </select>
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          </div>
+          <div class="themed">
+            <fieldset>
+              <legend>Most Recent Upload</legend>
+              <div class="full_width_block">
+                
+              </div>
+            </fieldset>
+          </div>
+          
+        </div>
 
       </div>
     </div>
