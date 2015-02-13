ae9593aada2c2a0227edc5db538870bf27f0ae93
Ken Auberry <kenneth.auberry@pnnl.gov>
2015-02-12 16:22:32 -0800

    Changes to allow use of myemsl/general.ini file for database login info

--- application/config/database.php
+++ application/config/database.php
@@ -48,21 +48,36 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'sqlite:'.APPPATH.'config/database/myemsl_status_site_info.sqlite:';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'pdo';
-$db['default']['dbprefix'] = '';
+// $db['default']['hostname'] = 'sqlite:'.APPPATH.'config/database/myemsl_status_site_info.sqlite:';
+// $db['default']['username'] = '';
+// $db['default']['password'] = '';
+// $db['default']['database'] = '';
+// $db['default']['dbdriver'] = 'pdo';
+// $db['default']['dbprefix'] = '';
+// $db['default']['pconnect'] = TRUE;
+// $db['default']['db_debug'] = TRUE;
+// $db['default']['cache_on'] = FALSE;
+// $db['default']['cachedir'] = '';
+// $db['default']['char_set'] = 'utf8';
+// $db['default']['dbcollat'] = 'utf8_general_ci';
+// $db['default']['swap_pre'] = '';
+// $db['default']['autoinit'] = TRUE;
+// $db['default']['stricton'] = FALSE;
+
+$myemsl_array = parse_ini_file("/etc/myemsl/general.ini", TRUE);
+
+$db['default']['hostname'] = $myemsl_array['metadata']['host'];
+$db['default']['username'] = $myemsl_array['metadata']['user'];
+$db['default']['password'] = $myemsl_array['metadata']['password'];
+$db['default']['database'] = $myemsl_array['metadata']['database'];
+$db['default']['dbdriver'] = "mysql";
+$db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
-$db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
+$db['default']['cachedir'] = "";
+
+
 
 
 $db['ws_info']['hostname'] = 'sqlite:'.APPPATH.'config/database/myemsl_status_site_info.sqlite:';
