44dfd738f24fbe783709a0a8a8325be5c5360c05
Ken Auberry <kenneth.auberry@pnnl.gov>
2015-04-29 12:00:29 -0700

    Added loading spinner to instrument selector

--- resources/scripts/status_common.js
+++ resources/scripts/status_common.js
@@ -2,6 +2,28 @@ var trans_id_list = {};
 var latest_tx_id = 0;
 var inst_id = 0;
 
+var spinner_opts = {
+  lines: 9, // The number of lines to draw
+  length: 4, // The length of each line
+  width: 3, // The line thickness
+  radius: 4, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#111', // #rgb or #rrggbb or array of colors
+  speed: 1, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: true, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: '50%', // Top position relative to parent
+  left: '50%' // Left position relative to parent
+};
+
+
+
+
 $(function(){
   inst_id = $('#instrument_selector').length > 0 ? $('#instrument_selector').val() : initial_inst_id;
 });
@@ -93,6 +115,8 @@ var update_content = function(event){
 
 var get_instrument_list = function(proposal_id){
   var inst_url = base_url + 'index.php/status/get_instrument_list/' + proposal_id;
+  var target = document.getElementById('instrument_selector_spinner');
+  var spinner = new Spinner(spinner_opts).spin(target);
   $.getJSON(inst_url,function(data){
     $('#instrument_selector').select2({
       data: data.items,
@@ -107,6 +131,7 @@ var get_instrument_list = function(proposal_id){
     if(initial_instrument_list.indexOf(parseInt(initial_instrument_id,10)) < 0){
       $('#instrument_selector').val('').trigger('change');
     }
+    spinner.stop();
   });
 };
 
