097c2fba1fed9090aaa68305a82a118a61011e24
Ken Auberry <kenneth.auberry@pnnl.gov>
2016-03-18 15:16:52 -0700

    Test change

--- application/controllers/status.php
+++ application/controllers/status.php
@@ -15,20 +15,20 @@ class Status extends Baseline_controller {
       3 => 'Verified', 4 => 'Stored', 5 => 'Available', 6 => 'Archived'
     );
     $this->valid_search_term_types = array(
-            
+      
     );
-    
+
     $this->last_update_time = get_last_update(realpath(BASEPATH.'../'.APPPATH.'/'));
   }
-  
+
   public function index(){
     redirect('status/overview');
   }
-  
+
   public function xml($search_term_type,$filter){
-    
+
   }
-  
+
   public function view($lookup_type,$id = -1){
     $valid_lookup_types = array(
       'j' => 'j','job' => 'j','t' => 't','transaction' => 't'
@@ -46,7 +46,7 @@ class Status extends Baseline_controller {
       $this->load->view('status_error_page.html',$this->page_data);
       // redirect(base_url()."index.php/status/view/{$lookup_type}/{$id}");
     }
-    
+
     $lookup_type_description = array_key_exists($lookup_type,$lookup_type_descriptions) ? $lookup_type_descriptions[$lookup_type] : 'job';
     if(!is_numeric($id) || $id < 0){
       //that doesn't look like a real id
@@ -55,8 +55,8 @@ class Status extends Baseline_controller {
       $this->page_data['lookup_type_desc'] = $lookup_type_description;
       $this->page_data['lookup_type'] = $lookup_type;
       $this->load->view('status_error_page.html', $this->page_data);
-    }    
-    
+    }
+
     $this->page_data['page_header'] = "Upload Report";
     $this->page_data['title'] = "Upload Report";
     $this->page_data['css_uris'] = array(
@@ -76,9 +76,9 @@ class Status extends Baseline_controller {
       "/resources/scripts/myemsl_file_download.js",
       "/resources/scripts/status_common.js",
       "/resources/scripts/moment.min.js",
-      "/resources/scripts/single_item_view.js"    
+      "/resources/scripts/single_item_view.js"
     );
-    
+
     if($lookup_type == 'j' || $lookup_type == 'job'){
       //lookup transaction_id from job
       $tx_info = $this->status->get_transaction_info($id);
@@ -94,11 +94,11 @@ class Status extends Baseline_controller {
         $this->page_data['transaction_data'] = $job_status_info;
       }
     }else{
-      $this->page_data['transaction_sizes'] = $this->status->get_total_size_for_transactions(array($id));      
+      $this->page_data['transaction_sizes'] = $this->status->get_total_size_for_transactions(array($id));
       $inst_id = $this->status->get_instrument_for_id('t',$id);
       $transaction_list = array();
       $transaction_list[] = $id;
-      
+
       $transaction_info = $this->status->get_formatted_object_for_transactions($transaction_list);
       if(empty($transaction_info)){
         $this->page_data['message'] = "No {$lookup_type_description} with an identifier of {$id} was found";
@@ -106,9 +106,9 @@ class Status extends Baseline_controller {
       }
       $this->page_data['transaction_data'] = $transaction_info;
     }
-    
+
     $this->page_data['cart_data'] = array('carts' => $this->cart->get_active_carts($this->user_id, false));
-    
+
     $this->page_data['request_type'] = $lookup_type;
     $this->page_data['enable_breadcrumbs'] = true;
     $this->page_data['js'] = "var initial_inst_id = '{$inst_id}';
@@ -120,7 +120,7 @@ var email_address = '{$this->email}';
 
 }
 
-  
+
   public function overview($proposal_id = false, $instrument_id = false, $time_period = false){
     $time_period = $this->input->cookie('myemsl_status_last_timeframe_selector') ? $this->input->cookie('myemsl_status_last_timeframe_selector') : $time_period;
     $instrument_id = $this->input->cookie('myemsl_status_last_instrument_selector') ? $this->input->cookie('myemsl_status_last_instrument_selector') : $instrument_id;
@@ -136,7 +136,7 @@ var email_address = '{$this->email}';
         "/resources/stylesheets/status_style.css",
         "/resources/scripts/select2/select2.css",
         "/resources/stylesheets/file_directory_styling.css",
-        "/resources/stylesheets/bread_crumbs.css"      
+        "/resources/stylesheets/bread_crumbs.css"
       );
       $this->page_data['script_uris'] = array(
         "/resources/scripts/spinner/spin.min.js",
@@ -159,16 +159,16 @@ var email_address = '{$this->email}';
         }
       }
       krsort($proposal_list);
-      
+
       $js = "var initial_proposal_id = '{$proposal_id}';
 var initial_instrument_id = '{$instrument_id}';
 var initial_time_period = '{$time_period}';
 var email_address = '{$this->email}';
 var lookup_type = 't';
 var initial_instrument_list = [];";
-      
+
       $this->page_data['proposal_list'] = $proposal_list;
-      
+
       $this->page_data['load_prototype'] = false;
       $this->page_data['load_jquery'] = true;
       $this->page_data['selected_proposal'] = isset($proposal_id) ? $proposal_id : false;
@@ -192,7 +192,7 @@ var initial_instrument_list = [];";
       }elseif($instrument_id < 0){
         //this should be the "all instruments" trigger
         //  get all the instruments for this proposal
-        
+
         $results = array(
           'transaction_list' => array(),
           'time_period_empty' => false,
@@ -219,20 +219,20 @@ var initial_instrument_list = [];";
                 $results['transaction_list']['times'][$ts] = $tx_id;
               }
             }
-            
+
           }
         }
       }else{
         $results = array(
-          'transaction_list' => array(), 
-          'time_period_empty' => true, 
+          'transaction_list' => array(),
+          'time_period_empty' => true,
           'message' => "No data uploaded for this instrument"
         );
       }
     }else{
       $results = array(
-        'transaction_list' => array(), 
-        'time_period_empty' => true, 
+        'transaction_list' => array(),
+        'time_period_empty' => true,
         'message' => "No data uploaded for this instrument"
       );
     }
@@ -251,11 +251,11 @@ var initial_instrument_list = [];";
     }
     $this->page_data['informational_message'] = $results['message'];
     $this->page_data['request_type'] = 't';
-     
+
     $this->load->view($view_name,$this->page_data);
   }
-  
-  
+
+
   public function get_files_by_transaction($transaction_id = false){
     if(!isset($transaction_id) || !$transaction_id){
       $output_array = array();
@@ -265,15 +265,15 @@ var initial_instrument_list = [];";
     }
     transmit_array_with_json_header($output_array);
   }
-  
-  
-  
+
+
+
   public function get_latest_transactions($instrument_id = "",$proposal_id = "",$latest_id = ""){
     $group_list = $this->status->get_instrument_group_list();
     $new_transactions = array();
     if(array_key_exists($instrument_id,$group_list['by_inst_id'])){
-      $new_transactions = $this->status->get_latest_transactions(array_keys($group_list['by_inst_id'][$instrument_id]),$proposal_id,$latest_id);    
-    }    
+      $new_transactions = $this->status->get_latest_transactions(array_keys($group_list['by_inst_id'][$instrument_id]),$proposal_id,$latest_id);
+    }
     if(empty($new_transactions)){
       print "";
       return;
@@ -283,8 +283,8 @@ var initial_instrument_list = [];";
     foreach($group_list['groups'] as $tx_id => $group_info){
       $results['transactions'][$tx_id]['groups'] = $group_info;
     }
-      
-    
+
+
     $this->page_data['status_list'] = $this->status_list;
     $this->page_data['transaction_data'] = $results;
     $view_name = 'upload_item_view.html';
@@ -295,7 +295,7 @@ var initial_instrument_list = [];";
       print "";
     }
   }
-  
+
   public function get_status($lookup_type, $id = 0){
     //lookup by (j)ob or (t)ransaction
     //check for list of transactions in post
@@ -308,9 +308,9 @@ var initial_instrument_list = [];";
     $item_keys = array_keys($item_list);
     sort($item_keys);
     $last_id = array_pop($item_keys);
-        
+
     $status_info = array();
-    
+
     $status_obj = $this->status->get_status_for_transaction($lookup_type,array_keys($item_list));
     if(!empty($status_obj)){
       foreach($status_obj as $item_id => $item_info){
@@ -339,7 +339,7 @@ var initial_instrument_list = [];";
       }elseif(sizeof($status_info) == 1){
         $this->load->view('status_breadcrumb_insert_view.html',$status_info[$id]);
       }
-      
+
     }
   }
 
@@ -364,8 +364,8 @@ var initial_instrument_list = [];";
     $results = $this->status->get_job_status($values, $this->status_list);
     transmit_array_with_json_header($results);
   }
-  
-  
+
+
   public function get_instrument_list($proposal_id){
     // $instruments = $this->eus->get_instruments_for_proposal($proposal_id);
     $full_user_info = $this->myemsl->get_user_info();
@@ -375,12 +375,12 @@ var initial_instrument_list = [];";
     foreach($instruments_available as $inst_id){
       $instruments[$inst_id] = "Instrument {$inst_id}: {$full_user_info['instruments'][$inst_id]['eus_display_name']}";
     }
-    
+
     asort($instruments);
-    
+
     format_array_for_select2(array('items' => $instruments));
   }
-  
+
   public function get_instrument_info($eus_instrument_id = 0){
     $results = array();
     if($eus_instrument_id){
@@ -388,43 +388,43 @@ var initial_instrument_list = [];";
     }
     transmit_array_with_json_header($results);
   }
-  
+
   public function test_get_status($job_id){
     var_dump($this->status->get_status_for_transaction('j',$job_id));
   }
-  
+
   public function test_get_instrument_list($instrument_id = ""){
     var_dump($this->status->get_instrument_group_list($instrument_id));
   }
-  
+
   public function test_get_groups_for_proposal($proposal_id){
     $results = $this->status->get_proposal_group_list($proposal_id);
     var_dump($results);
   }
-  
+
   public function test_get_groups_for_transaction($transaction_id){
     $results = $this->status->get_groups_for_transaction($transaction_id);
     echo "<pre>";
     var_dump($results);
     echo "</pre>";
   }
-  
+
   public function test_get_transactions_for_proposal($proposal_id){
     $results = $this->status->get_transactions_for_group(-1, 30,$proposal_id);
     var_dump($results);
   }
-  
+
   public function test_get_userinfo(){
     $user_info = $this->myemsl->get_user_info();
     // var_dump($user_info);
   }
-  
+
   public function test_get_proposals_for_instrument($instrument_id){
     $inst_list = $this->eus->get_proposals_for_instrument($instrument_id);
     var_dump($inst_list);
   }
-  
-  
+
+
 }
 
 ?>
