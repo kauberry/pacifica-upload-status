a20ee8b22ffb86973926b4ae27bfe0af51748271
Ken Auberry <kenneth.auberry@pnnl.gov>
2016-03-21 16:59:33 -0700

    Making conversion from CI 2.1.x => 3.0.5

--- application/config/config.php
+++ application/config/config.php
@@ -48,7 +48,7 @@ $config['index_page'] = 'index.php';
 | 'ORIG_PATH_INFO'	Uses the ORIG_PATH_INFO
 |
 */
-$config['uri_protocol']	= 'PATH_INFO';
+$config['uri_protocol']	= 'AUTO';
 
 /*
 |--------------------------------------------------------------------------
@@ -184,7 +184,7 @@ $config['directory_trigger']	= 'd'; // experimental not currently in use
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 4;
 
 /*
 |--------------------------------------------------------------------------
