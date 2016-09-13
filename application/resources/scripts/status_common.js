486ee613eeb69e5b928ecbeab6e5efed1a98e303
Ken Auberry <kauberry@mac.com>
2016-09-13 13:09:19 -0700

    Fix #28 (#29)
    Re-coded the matching algorithm to look at the correct parts of the Select Option item

--- application/resources/scripts/status_common.js
+++ application/resources/scripts/status_common.js
@@ -171,6 +171,7 @@ var get_instrument_list = function(proposal_id){
       placeholder: "Select an Instrument...",
       templateResult: formatInstrument,
       templateSelection: formatInstrumentSelection,
+      matcher: my_matcher,
       escapeMarkup: function(markup) {
           return markup;
       }
@@ -199,12 +200,12 @@ var formatInstrument = function(item){
     var active = item.active == 'Y' ? 'active' : 'inactive';
     if(item.id){
         if(item.id > 0){
-            markup = "<div id='inst_info_" + item.id + "' class='inst_info'>";
+            markup =  "<div id='inst_info_" + item.id + "' class='inst_info'>";
             markup += "  <div class='" + active + "_instrument'>";
             markup += "     <strong>Instrument " + item.id + "</strong>";
             markup += "  </div>";
+            markup += "  <div class='inst_description'>" + item.name + "</div>";
             markup += "</div>";
-            markup += "<div class='inst_description'>" + item.name + "</div>";
         }else if (item.id == -1) {
             markup = "<div id='inst_info_" + item.id + "' class='inst_info'>";
             markup += "<strong>All Instruments for Proposal " + current_proposal_id + "</strong>";
@@ -361,3 +362,36 @@ var get_tree_data = function(event, data){
   // setup_file_download_links($(el));
   // var url =
 };
+
+function my_matcher(params, data){
+    // Always return the object if there is nothing to compare
+    // data.text should only be blank for the placeholder, return the item
+    if ($.trim(params.term) === '' || $.trim(data.text) === '') {
+      return data;
+    }
+
+    var original = data.text.toUpperCase();
+    var termstring = params.term.toUpperCase();
+
+    var terms = termstring.split(' ');
+    terms = $.isArray(terms) ? terms : [terms];
+    var is_match = false;
+
+    $.each(terms, function(index, term){
+        // Check if the text contains the term
+        if (original.indexOf(term) > -1) {
+            is_match = true;
+        }else{
+            is_match = false;
+        }
+        return is_match;
+    });
+
+    if(is_match === true){
+        return data;
+    }
+
+    // If it doesn't contain the term, don't return anything
+    return null;
+
+}
