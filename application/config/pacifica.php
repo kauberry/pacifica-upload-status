53462479a66cbb3403042ba97b3cd13c81ccf469
David Brown <dmlb2000@gmail.com>
2017-01-26 16:19:28 -0800

    Issue 74 demos changes (#77)
    * Fixing pacifica.php config file
    
    * More Demos changes
    
    * Wrong damn place
    
    * Config file fix
    
    * Config file issue fixed

--- application/config/pacifica.php
+++ application/config/pacifica.php
@@ -15,50 +15,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['local_timezone'] = 'America/Los_Angeles';
 
-$config['internal_cart_url'] = !empty(getenv('CART_PORT')) ?
-    str_replace('tcp://', 'http://', getenv('CART_PORT')) :
+$cart_port = getenv('CART_PORT');
+$cart_dl_port = getenv('CART_DOWNLOAD_PORT');
+
+$config['internal_cart_url'] = !empty($cart_port) ?
+    str_replace('tcp://', 'http://', $cart_port) :
     'http://cart:8081';
 
-$config['external_cart_url'] = !empty(getenv('CART_PORT')) ?
-    str_replace('tcp://', 'http://', getenv('CART_DOWNLOAD_PORT')) :
+$config['external_cart_url'] = !empty($cart_dl_port) ?
+    str_replace('tcp://', 'http://', $cart_dl_port) :
     'http://download.my.emsl.pnl.gov';
 
 $config['template'] = 'emsl';
 $config['site_color'] = 'orange';
 
 $config['application_version'] = "1.99.0";
-?>
-
-
-<?php
-/**
- * CI Default Pacifica Config
- *
- * PHP Version 5
- *
- * @category Configuration
- * @package  Default_Pacifica
- * @author   Ken Auberry <Kenneth.Auberry@pnnl.gov>
- * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     http://github.com/EMSL-MSC/pacifica-upload-status
- */
-
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['local_timezone'] = 'America/Los_Angeles';
-
-$config['internal_cart_url']
-    = !empty(getenv('CART_PORT')) ?
-    str_replace('tcp://', 'http://', getenv('CART_PORT')) :
-    'http://cart:8081';
-
-$config['external_cart_url']
-    = !empty(getenv('CART_PORT')) ?
-    str_replace('tcp://', 'http://', getenv('CART_DOWNLOAD_PORT')) :
-    'http://download.my.emsl.pnl.gov';
-
-$config['template'] = 'emsl';
-$config['site_color'] = 'orange';
-
-$config['application_version'] = "0.99.11";
 ?>
