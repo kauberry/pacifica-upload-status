5f4c153004162b891ae1c8bb7192426f1a46b06d
dmlb2000 <dmlb2000@gmail.com>
2017-05-12 15:10:28 -0700

    add version bump

--- application/config/pacifica.php
+++ application/config/pacifica.php
@@ -23,5 +23,5 @@ $config['application_config_file_path'] = "/etc/myemsl/";
 
 $config['template'] = "emsl";
 
-$config['application_version'] = "0.99.12";
+$config['application_version'] = "0.99.13";
 ?>
